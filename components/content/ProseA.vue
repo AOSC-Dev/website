92a63f089e81266ecc3cf30e882060a30f26fea0
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-01-25 21:38:11 +0800

    fix: locale is ignored for links in markdown files

---
+++ components/content/ProseA.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppLink
+    :href="props.href"
+    :target="props.target"
+  >
+    <slot />
+  </AppLink>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+// https://github.com/nuxt-content/mdc/blob/main/src/runtime/components/prose/ProseA.vue
+const props = defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
+    default: undefined,
+    required: false,
+  },
+})
+</script>
