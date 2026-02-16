8b181ea0e149692893b6a93c4970ec324bb9e331
Mingcong Bai <jeffbai@aosc.io>
2026-02-16 23:31:35 +0800

    feat: 上线春节配色

--- composables/useDefaultHead.ts
+++ composables/useDefaultHead.ts
@@ -10,7 +10,8 @@ export default () => {
         // This script will execute before the browser renders the <body>,
         // ensuring the correct season class is set from the very beginning.
         textContent:
-          'document.documentElement.classList.add(["spring","summer","autumn","winter"][(new Date().getMonth()+10)%12/3|0])',
+          //'document.documentElement.classList.add(["spring","summer","autumn","winter"][(new Date().getMonth()+10)%12/3|0])',
+          'document.documentElement.classList.add(["festive"])',
         type: 'text/javascript',
         tagPosition: 'head'
       }
