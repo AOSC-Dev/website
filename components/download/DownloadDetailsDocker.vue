b688ba7698e347449093449d15a5c90f8a8b976b
dongdigua <dongdigua@outlook.com>
2026-02-20 20:49:59 +0800

    feat(download): add docker tab

---
+++ components/download/DownloadDetailsDocker.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { t } = useI18n();
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <p>
+      {{ t('DownloadDetailsDocker.text1') }}
+      <AppLink to="https://hub.docker.com/r/aosc/aosc-os" target="_blank">Docker Hub</AppLink>
+      {{ t('DownloadDetailsDocker.text2') }}
+    </p>
+
+    <p>{{ t('DownloadDetailsDocker.text3') }}</p>
+    <CopyButton :content="`docker pull aosc/aosc-os`"/>
+  </div>
+</template>
+
+<style scoped></style>
