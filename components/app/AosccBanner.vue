52ae7a48f3a0b1fd161a20bed8231adbcf2a0c80
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-03-31 23:21:03 +0800

    feat: add farts

--- components/app/AosccBanner.vue
+++ components/app/AosccBanner.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
+import { farts, Fart } from '~/assets/scripts/fart.js';
 import { Close } from '@element-plus/icons-vue';
 import { ElButton } from 'element-plus';
 import { ref } from 'vue';
 
+let fart: InstanceType<typeof Fart> | null = null;
+onMounted(() => (fart = new Fart({})));
+const randomFart = () => {
+  if (!fart) return;
+  fart.play(
+    Object.values(farts)[
+      Math.floor(Math.random() * (Object.keys(farts).length + 1))
+    ],
+    undefined
+  );
+};
+
 const showBanner = ref(true);
-const endTime = Date.parse('2026/7/6');
-function deltaTime(timestamp1 = Date.now(), timestamp2 = endTime) {
-  let delta = Math.abs(timestamp2 - timestamp1) / 1000;
-  const days = Math.floor(delta / 86400);
-  delta -= days * 86400;
-  const hours = Math.floor(delta / 3600) % 24;
-  return [days, hours];
-}
+const endTime = Date.parse('2026/4/2');
+const deltaTime = (timestamp1 = Date.now(), timestamp2 = endTime) => {
+  const totalSeconds = Math.max(
+    0,
+    Math.floor((timestamp2 - timestamp1) / 1000)
+  );
+  const days = Math.floor(totalSeconds / 86400);
+  const hours = Math.floor((totalSeconds % 86400) / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  return days
+    ? `${days} days ${hours} hours`
+    : `${hours} hours ${minutes} minutes`;
+};
 </script>
 
 <template>
@@ -26,17 +44,27 @@ function deltaTime(timestamp1 = Date.now(), timestamp2 = endTime) {
       <br />
       Meet more AnAn Fumos than you can ever imagine
     </div>
-    <el-button type="primary" size="large" round class="text-[16px]">
+    <el-button
+      type="primary"
+      size="large"
+      round
+      class="text-[16px]"
+      @click="randomFart">
       Get Offer
     </el-button>
     <div>
       <div class="text-[11px]">ENDS IN</div>
-      {{ deltaTime()[0] }} Days {{ deltaTime()[1] }} hours
+      {{ deltaTime() }}
     </div>
     <el-button
       :icon="Close"
       circle
       class="absolute right-4"
-      @click="showBanner = false" />
+      @click="
+        () => {
+          randomFart();
+          showBanner = false;
+        }
+      " />
   </div>
 </template>
