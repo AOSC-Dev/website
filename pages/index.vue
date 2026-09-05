c79390402cf2a1447421175685e5a131c6f25cda
Mingcong Bai <jeffbai@aosc.io>
2026-09-05 11:51:01 +0800

    Revert "feat(pages/index): 临时更换头图为 AOSCC 2026 入口"
    This reverts commit 234a5e15e8a83ea6f1ad0d6e94015d747e255d58.

--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,8 @@ const securityFilter = [{ key: 'categories', value: '%"security"%' }];
 <template>
   <div class="flex">
     <div class="w-[62.5%] bg-content-main-bg p-0">
-      <app-link to="/aoscc/2026" class="flex w-full">
-        <img class="imgScale1 w-full" src="/jumbotron/main1.webp" />
+      <app-link to="/download#aosc-os-download" class="flex w-full">
+        <img class="imgScale1 w-full" src="/jumbotron/main1.svg" />
       </app-link>
       <!-- 资讯要点 -->
       <div>
